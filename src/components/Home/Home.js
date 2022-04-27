d3467928c6d8c0913450c7b90f6e336d5f5cbe59
Burak <mburak.demir.059@gmail.com>
2022-04-28 00:21:43 +0300

    a

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useRef,useEffect} from 'react'
 import Transition from '../Transition'
 import "./Home.css"
+import gsap from "gsap"
 const Home = () => {
+  const home=gsap.timeline();
+
+  const homeh1=useRef(null);
+
+  const homeimg=useRef(null);
+  useEffect(()=>{
+    
+
+  },[])
   return (
     <div>
-      <Transition/>
-      <div className="home-image home-overlay"></div>
+      <Transition timeline={home}/>
+      <div className="home-image home-overlay" ref={homeimg}></div>
       <div className="container-home">
-        <h1>Home Page</h1>
+        <h1 ref={homeh1}>Home Page</h1>
       </div>
     </div>
   )
--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useRef,useEffect} from 'react'
 import "./Transition.css"
-const Transition = () => {
-  return (
-    <div>
-        <div className="transition-effect"></div>
-    </div>
-  )
-}
+import {Power4} from "gsap";
+const Transition = ({timeline}) => {
+    
+    const trans=useRef(null);
+    useEffect(()=>{
+        timeline.to(trans.current,{
+            duration:1,
+            y:2600,
+            ease:Power4.easeIn
+        })
+
+    })
+
+    return (
+        <div>
+            <div className="transition-effect" ref={trans}></div>
+        </div>
+    )
+    }
 
 export default Transition
