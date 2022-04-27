cc6694eaeefbfd8e66f8f653d93588e36e26707a
Burak <mburak.demir.059@gmail.com>
2022-04-27 23:43:57 +0300

    Transition Page Setup

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Transition from '../Transition'
 import "./Home.css"
 const Home = () => {
   return (
     <div>
+      <Transition/>
       <div className="home-image home-overlay"></div>
       <div className="container-home">
         <h1>Home Page</h1>
---
+++ src/components/Transition.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Transition.css"
+const Transition = () => {
+  return (
+    <div>
+        <div className="transition-effect"></div>
+    </div>
+  )
+}
+
+export default Transition
